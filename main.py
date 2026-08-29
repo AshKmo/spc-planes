a8d6ad4b8ea722b3448ccc84895c580b7fe78696
Zain Khan <contact@zain-khan.dev>
2026-08-29 15:53:23 +1000

    succesfully updated to use dictionaries

--- main.py
+++ main.py
@@ -18,16 +18,18 @@ image_plane2 = pygame.image.load("assets/plane2.png")
 
 image_bullet = pygame.image.load("assets/bullet.png")
 
+
 class Plane:
     def __init__(self, name, image, p, r, ai_f):
         self.image = image
         self.p = p
         self.r = r
-        self.ai_f = ai_f or (lambda *x : (10, True))
+        self.ai_f = ai_f or (lambda *x: (10, True))
         self.shooting = False
         self.shoot_cooldown = 0
         self.health = 5
         self.name = name
+
 
 class Bullet:
     def __init__(self, p, r):
@@ -35,6 +37,7 @@ class Bullet:
         self.r = r
         self.hit = False
         self.life = 100
+
 
 pygame.init()
 
@@ -44,13 +47,19 @@ clock = pygame.time.Clock()
 
 running = True
 
-plane1 = Plane("PLANE1 (BLUE)", image_plane1, pygame.math.Vector2(100, 300), 0, plane1_ai.plane_ai)
-plane2 = Plane("PLANE2 (RED)", image_plane2, pygame.math.Vector2(700, 300), 180, plane2_ai.plane_ai)
+plane1 = Plane(
+    "PLANE1 (BLUE)", image_plane1, pygame.math.Vector2(100, 300), 0, plane1_ai.plane_ai
+)
+plane2 = Plane(
+    "PLANE2 (RED)", image_plane2, pygame.math.Vector2(700, 300), 180, plane2_ai.plane_ai
+)
 
 planes = [plane1, plane2]
 
+
 def flip_angle(a):
     return (MAX_ANGLE / 2 - a) % 360
+
 
 bullets = []
 
@@ -59,8 +68,28 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    (plane1.r, plane1.shooting) = plane1.ai_f(SimpleNameSpace(**{'x': plane1.p.x, 'y': plane1.p.y, 'rot': plane1.r}), SimpleNamespace(**{'x': plane2.p.x, 'y': plane2.p.y, 'rot': plane2.r}), [(b.p, b.r) for b in bullets])
-    (plane2.r, plane2.shooting) = plane2.ai_f(SimpleNameSpace(**{GAME_WIDTH - plane2.p.x, plane2.p.y, flip_angle(plane2.r)}), SimpleNameSpace(**{GAME_WIDTH - plane1.p.x, plane1.p.y, flip_angle(plane1.r)}), [(GAME_WIDTH - b.p.x, b.p.y, flip_angle(b.r)) for b in bullets])
+    (plane1.r, plane1.shooting) = plane1.ai_f(
+        SimpleNamespace(**{"x": plane1.p.x, "y": plane1.p.y, "rot": plane1.r}),
+        SimpleNamespace(**{"x": plane2.p.x, "y": plane2.p.y, "rot": plane2.r}),
+        [(b.p, b.r) for b in bullets],
+    )
+    (plane2.r, plane2.shooting) = plane2.ai_f(
+        SimpleNamespace(
+            **{
+                "x": GAME_WIDTH - plane2.p.x,
+                "y": plane2.p.y,
+                "rot": flip_angle(plane2.r),
+            }
+        ),
+        SimpleNamespace(
+            **{
+                "x": GAME_WIDTH - plane1.p.x,
+                "y": plane1.p.y,
+                "rot": flip_angle(plane1.r),
+            }
+        ),
+        [(GAME_WIDTH - b.p.x, b.p.y, flip_angle(b.r)) for b in bullets],
+    )
     plane1.r = plane1.r % 360
     plane2.r = flip_angle(plane2.r)
 
@@ -69,19 +98,27 @@ while running:
     for plane in planes:
         plane.p = plane.p + pygame.math.Vector2.from_polar((4, -plane.r))
 
-        plane.p.update(min(max(plane.p.x, 0), GAME_WIDTH), min(max(plane.p.y, 0), GAME_HEIGHT))
+        plane.p.update(
+            min(max(plane.p.x, 0), GAME_WIDTH), min(max(plane.p.y, 0), GAME_HEIGHT)
+        )
 
         if plane.shooting and plane.shoot_cooldown == 0:
             plane.shoot_cooldown = 60
 
-            bullets.append(Bullet(plane.p + pygame.math.Vector2.from_polar((PLANE_SIZE + 5, -plane.r)), plane.r))
+            bullets.append(
+                Bullet(
+                    plane.p
+                    + pygame.math.Vector2.from_polar((PLANE_SIZE + 5, -plane.r)),
+                    plane.r,
+                )
+            )
 
         if plane.shoot_cooldown > 0:
             plane.shoot_cooldown -= 1
 
         screen.blit(
             pygame.transform.rotate(plane.image, plane.r),
-            (plane.p.x - 25, plane.p.y - 25)
+            (plane.p.x - 25, plane.p.y - 25),
         )
 
     for bullet in bullets:
@@ -91,7 +128,7 @@ while running:
 
         screen.blit(
             pygame.transform.rotate(image_bullet, bullet.r),
-            (bullet.p.x - 5, bullet.p.y - 5)
+            (bullet.p.x - 5, bullet.p.y - 5),
         )
 
     for bullet in bullets:
@@ -100,7 +137,12 @@ while running:
                 plane.health -= 1
                 bullet.life = 0
 
-    bullets = [b for b in bullets if b.life > 0 and pygame.Rect(0, 0, GAME_WIDTH, GAME_HEIGHT).collidepoint(b.p.x, b.p.y)]
+    bullets = [
+        b
+        for b in bullets
+        if b.life > 0
+        and pygame.Rect(0, 0, GAME_WIDTH, GAME_HEIGHT).collidepoint(b.p.x, b.p.y)
+    ]
 
     planes = [p for p in planes if p.health > 0]
 
